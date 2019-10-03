8da3bbf0013c143f02238464fd2f2c8525dd536f
matthew <matheusrp97@hotmail.com>
2019-10-03 20:55:29 -0300

    Ate-agora

--- Jogo/jogoLogic.js
+++ Jogo/jogoLogic.js
@@ -1,6 +1,10 @@
 let ponto = 0;
 let IntervarlId;
 let timer = 30;
+let start = 1;
+let players = [];
+let player;
+let ranking = [];
 
 function randomImg() {
     let img, img2, topx, lefty;
@@ -28,9 +32,21 @@ function randomImg() {
 
 function pontos() {
     let points;
+    let finalPoints;
     ponto++;
     points = document.getElementsByClassName('points');
+    finalPoints = document.getElementsByClassName('pointsR');
     points[0].innerText = `Pontos: ${ponto}`;
+    finalPoints[0].innerText = `Seus pontos: ${ponto}`
+}
+
+function getPlayerandPoints() {
+    let pontos = ponto;
+    players.push({
+        player: player,
+        pontos: pontos
+    });
+    localStorage.setItem('player', players);
 }
 
 function tiraPontos() {
@@ -53,26 +69,70 @@ function sumir(img) {
 
 }
 
-function times() {
-    let time;
-    let ending =30;
-    time = document.getElementsByClassName('timer');
-    // ending = time[0].innerText.slice(15, 18);
-    // ending = parseInt(ending);
-    while(ending !== 0) {
-        IntervarlId = setInterval(() => {
-            timer -= 1;
-            ending -=1;
-            console.log(timer);
-            time[0].innerText = `Tempo Restante: ${timer}s`
-        }, 1000)
-    }
+
+function stopImages() {
+    let img = document.getElementById('img');
+    img.remove();
+    // endBoard();
 }
 
-document.addEventListener("DOMContentLoaded", function (event) {
-    // lembrar de mudar velocidade para mais rápido
+function endBoard() {
+    let pontos = document.getElementById('pointsboard');
+    pontos.remove();
+
+    ranking = localStorage.getItem('player');
+    console.log(ranking);
+
+    let endPoints = document.getElementById('ranking');
+    endPoints.setAttribute('style', 'visibility:visible')
+}
+
+function end() {
+    clearInterval(IntervarlId);
+    stopImages();
+    getPlayerandPoints();
+    endBoard();
+}
+
+function times() {
+    let time;
+    let ending = 30;
+    time = document.getElementsByClassName('timer');
+    IntervarlId = setInterval(() => {
+        timer -= 1;
+        ending -= 1;
+        time[0].innerText = `Tempo Restante: ${timer}s`
+        if (ending === 0) {
+            console.log('acabou');
+            end();
+        }
+    }, 1000)
+}
+
+function starts() {
+    player = document.getElementById('tagName').value;
+    let name = document.getElementById('getName');
+    name.remove();
+    main();
+}
+
+// window.addEventListener('click', function (e) {
+//     main();
+// })
+
+// lembrar de mudar velocidade para mais rápido
+function main() {
+    let boardpoints = document.getElementById('pointsboard');
+    boardpoints.setAttribute('style', 'visibility:visible');
+
+    let remove = document.getElementsByTagName('h1');
+    remove[0].remove();
+
+
     setInterval(function () { randomImg() }, 1500);
-    // times();
+
+    times();
+
     document.querySelectorAll('.ball').forEach(bal => {
         bal.addEventListener('click', function () {
             sumir(this);
@@ -87,7 +147,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
             tiraPontos();
         }
     });
+}
 
-    // document.getElementsByTagName("html")[0].style.cursor = "url('http://wiki-devel.sugarlabs.org/images/e/e2/Arrow.cur'), auto";
 
-});
